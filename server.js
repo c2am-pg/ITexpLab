4ff01c9adf26d2f82fe7819ab4ced0e33d91a974
Cam <cadomako223@gmail.com>
2025-11-03 14:51:43 -0500

    Hopefuly a fix to the hyperlink issue

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express'); // turns express on 
-const userRouter= require('./routes/users');
 const app = express();// calling sets up server
+const userRouter= require('./routes/users');
+
 app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
 console.log('Here');
 res.render("index",{user: "Cam",});
 //res.status(500).send("Hello World");
+});//runs on acess
 
+ app.get('/users', (req,res)=>{
+    res.send('User List');
+});
+app.get('/users/new', (req,res)=>{
+    res.send('New User Form');
 });
 
-app.listen(3030);
-// port its ran on 
+app.listen(3030);// port its ran on 
