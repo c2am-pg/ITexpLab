fd06c077c369693f4c45befeecf82450a79a8e75
Cam <cadomako223@gmail.com>
2025-11-12 14:00:13 -0500

    added id param function

--- routes/users.js
+++ routes/users.js
@@ -8,13 +8,10 @@ const router = express.Router();
 //    res.send(`Getting User data: ${req.params.id}`);
 //});
 router.get('/new', (req,res)=>{
-    res.send('New User Form');
+   // res.send('New User Form');
 });
 
-//router.post('/',(re,res)=>{
-//});
-
-router.route("/:id").get('/:id',(req,res)=>{
+router.route("/:id").get((req,res)=>{
     res.send(`Getting User data: ${req.params.id}`);
 }).delete((req,res)=>{
     res.send(`Deleting User with id: ${req.params.id}`);
@@ -22,4 +19,8 @@ router.route("/:id").get('/:id',(req,res)=>{
     res.send(`Updating User with id: ${req.params.id}`);
 });
 
+router.param("id",(req,res,next,id)=>{
+    console.log(`Accessing User #${id}`);
+    next();
+});
 module.exports = router;
